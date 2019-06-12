3a3b5398080745f25e3cf02eb346eda21f8e4a0a
Bendik Solheim <hello@bendik.dev>
2019-06-12 22:20:38 +0200

    Simple program

---
+++ programs/plus.c
@@ -0,0 +1,6 @@
+long main() {
+    long a = 5;
+    long b = 6;
+    long answer = a + b;
+    return answer;
+}
---
+++ programs/plus.s
@@ -0,0 +1,25 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 14	sdk_version 10, 14
+	.intel_syntax noprefix
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	mov	qword ptr [rbp - 8], 5
+	mov	qword ptr [rbp - 16], 6
+	mov	rax, qword ptr [rbp - 8]
+	add	rax, qword ptr [rbp - 16]
+	mov	qword ptr [rbp - 24], rax
+	mov	rax, qword ptr [rbp - 24]
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+
+.subsections_via_symbols
