00b1aab3599e009e268891d82100beb58e89b9ca
Bendik Solheim <hello@bendik.dev>
2019-06-21 00:15:33 +0200

    Add arrow-kt for some functional goodness

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm") version "1.3.31"
+    kotlin("kapt") version "1.3.21"
 }
 
 group = "dev.bendik"
@@ -10,11 +11,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/arrow-kt/arrow-kt/")
+    maven("https://oss.jfrog.org/artifactory/oss-snapshot-local/")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("io.arrow-kt:arrow-core-data:0.9.1-SNAPSHOT")
+    implementation("io.arrow-kt:arrow-syntax:0.9.1-SNAPSHOT")
+    implementation("io.arrow-kt:arrow-optics:0.9.1-SNAPSHOT")
+
+    kapt("io.arrow-kt:arrow-meta:0.9.1-SNAPSHOT")
 }
 
 tasks.withType<KotlinCompile> {
