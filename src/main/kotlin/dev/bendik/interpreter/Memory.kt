67a36ff97a7ab7c4db91921d28ccd448b7e3f4c2
Bendik Solheim <hello@bendik.dev>
2019-06-21 20:57:12 +0200

    Move memory stuff into separate file

---
+++ src/main/kotlin/dev/bendik/interpreter/Memory.kt
@@ -0,0 +1,38 @@
+package dev.bendik.interpreter
+
+fun writeMemory(process: Process, address: Long, value: Long, size: Int): Process {
+    val ad = address.toInt()
+    val bytes = longToBytes(value)
+    for (i in 0 until size) {
+        process.memory[ad + i] = bytes[i]
+    }
+
+    return process
+}
+
+fun readMemory(process: Process, address: Long, size: Int): Long {
+    val ad = address.toInt()
+    val bytes = process.memory.slice(ad..(ad + size))
+    return bytesToLong(bytes.toByteArray())
+}
+
+private fun longToBytes(value: Long): ByteArray {
+    var v = value
+    val result = ByteArray(8)
+    for(i in 0 until 8) {
+        result[7 - i] = (v and 0xFF).toByte()
+        v = v shr 8
+    }
+
+    return result
+}
+
+private fun bytesToLong(value: ByteArray): Long {
+    var result = 0L
+    for (i in 0 until 8) {
+        result = result shl 8
+        result = result or (value[i].toLong() and 0xFF)
+    }
+
+    return result
+}
